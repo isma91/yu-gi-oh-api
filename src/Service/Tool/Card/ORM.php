6dc405e43bd85c3a47166508f81ce156fffcabf5
isma91 <ismaydogmus@gmail.com>
2024-03-09 13:06:44 +0100

    create ORM service for Card entity

---
+++ src/Service/Tool/Card/ORM.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Service\Tool\Card;
+
+
+use App\Entity\Card;
+use App\Repository\CardRepository;
+use App\Service\Tool\AbstractORM;
+use App\Service\Tool\ORMSearch;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ORM extends AbstractORM
+{
+    protected ORMSearch $ORMSearch;
+
+    public function __construct(EntityManagerInterface $em, CardRepository $repository)
+    {
+        $this->ORMSearch = new ORMSearch($repository);
+        parent::__construct($em, $repository);
+    }
+
+    /**
+     * @return ORMSearch
+     */
+    public function getORMSearch(): ORMSearch
+    {
+        return $this->ORMSearch;
+    }
+
+    /**
+     * @param string $uuid
+     * @return Card|null
+     */
+    public function findByUuid(string $uuid): ?Card
+    {
+        return $this->repository->findOneBy(["uuid" => $uuid]);
+    }
+}
