fa57486c4ed05592c05b85e462ba4de71588fd9b
isma91 <ismaydogmus@gmail.com>
2024-03-09 23:25:13 +0100

    create type service + controller

---
+++ src/Service/Type.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Service;
+
+use App\Service\Tool\Type\ORM as TypeORMService;
+use Exception;
+
+class Type
+{
+    private CustomGeneric $customGenericService;
+
+    private TypeORMService $typeORMService;
+
+    public function __construct(CustomGeneric $customGenericService, TypeORMService $typeORMService)
+    {
+        $this->customGenericService = $customGenericService;
+        $this->typeORMService = $typeORMService;
+    }
+
+    /**
+     * @return array[
+     *  "error" => string,
+     *  "errorDebug" => string,
+     *  "type" => array[mixed],
+     *  ]
+     */
+    public function getAll(): array
+    {
+        return $this->customGenericService->getAllOrInfo(
+            $this->typeORMService,
+            "type",
+            ["type_list"],
+            "Type"
+        );
+    }
+}
