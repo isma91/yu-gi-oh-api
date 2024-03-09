47d2fc2f95d8aa0c60c03f578975aa840e0f68ff
isma91 <ismaydogmus@gmail.com>
2024-03-09 23:23:59 +0100

    create category service + controller

---
+++ src/Service/Category.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Service;
+
+use App\Service\Tool\Category\ORM as CategoryORMService;
+use Exception;
+
+class Category
+{
+    private CustomGeneric $customGenericService;
+
+    private CategoryORMService $categoryORMService;
+
+    public function __construct(CustomGeneric $customGenericService, CategoryORMService $categoryORMService)
+    {
+        $this->customGenericService = $customGenericService;
+        $this->categoryORMService = $categoryORMService;
+    }
+
+    /**
+     * @return array[
+     *  "error" => string,
+     *  "errorDebug" => string,
+     *  "category" => array[mixed],
+     *  ]
+     */
+    public function getAll(): array
+    {
+        return $this->customGenericService->getAllOrInfo(
+            $this->categoryORMService,
+            "category",
+            ["category_list"],
+            "Category"
+        );
+    }
+}
