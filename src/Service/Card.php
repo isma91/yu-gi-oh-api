953e3c5eec6338944fc80c49e687cc677c135fbb
isma91 <ismaydogmus@gmail.com>
2024-03-17 23:46:34 +0100

    orm service of card can be get

--- src/Service/Card.php
+++ src/Service/Card.php
@@ -18,6 +18,14 @@ class Card
     }
 
     /**
+     * @return CardORMService
+     */
+    public function getORMService(): CardORMService
+    {
+        return $this->cardORMService;
+    }
+
+    /**
      * @param string $jwt
      * @param string $cardUuid
      * @return array[
