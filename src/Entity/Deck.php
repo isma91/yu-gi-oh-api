6f3b613b8db3823b1fdc0854ccc9a4e045a37e32
isma91 <ismaydogmus@gmail.com>
2024-03-17 23:46:21 +0100

    relation with Deck, DeckCard and Card

--- src/Entity/Deck.php
+++ src/Entity/Deck.php
@@ -3,35 +3,69 @@
 namespace App\Entity;
 
 use App\Repository\DeckRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
+use OpenApi\Attributes as OA;
 
+#[OA\Schema(
+    description: "A Deck is a list of Card with some specification."
+)]
 #[ORM\Entity(repositoryClass: DeckRepository::class)]
 class Deck
 {
+    use TimestampableEntity;
+    #[OA\Property(description: "Internal unique identifier of the Deck", type: "integer", nullable: false)]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(["deck_user_list"])]
     private ?int $id = null;
 
+    #[OA\Property(description: "Name of the Deck", type: "string", nullable: false)]
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(["deck_user_list"])]
     private ?string $name = null;
 
+    #[OA\Property(description: "Slugify name of the Deck", type: "string", nullable: false)]
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(["deck_user_list"])]
     private ?string $slugName = null;
 
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
-    private ?string $picture = null;
-
+    #[OA\Property(description: "If we authorize other user to see the Deck", type: "boolean", nullable: false)]
     #[ORM\Column]
+    #[Groups(["deck_user_list"])]
     private ?bool $isPublic = null;
 
     #[ORM\ManyToOne(inversedBy: 'decks')]
     #[ORM\JoinColumn(nullable: false)]
     private ?User $user = null;
 
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
-    private ?string $tag = null;
+    #[ORM\ManyToMany(targetEntity: Card::class, mappedBy: 'decks')]
+    private Collection $cardsUnique;
+
+    #[ORM\OneToMany(mappedBy: 'deck', targetEntity: CardMainDeck::class)]
+    private Collection $cardMainDecks;
+
+    #[ORM\OneToMany(mappedBy: 'deck', targetEntity: CardExtraDeck::class)]
+    private Collection $cardExtraDecks;
+
+    #[ORM\OneToMany(mappedBy: 'deck', targetEntity: CardSideDeck::class)]
+    private Collection $cardSideDecks;
+
+    #[ORM\ManyToOne]
+    private ?CardPicture $artwork = null;
+
+    public function __construct()
+    {
+        $this->cardsUnique = new ArrayCollection();
+        $this->cardMainDecks = new ArrayCollection();
+        $this->cardExtraDecks = new ArrayCollection();
+        $this->cardSideDecks = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -62,18 +96,6 @@ class Deck
         return $this;
     }
 
-    public function getPicture(): ?string
-    {
-        return $this->picture;
-    }
-
-    public function setPicture(?string $picture): static
-    {
-        $this->picture = $picture;
-
-        return $this;
-    }
-
     public function isIsPublic(): ?bool
     {
         return $this->isPublic;
@@ -98,14 +120,192 @@ class Deck
         return $this;
     }
 
-    public function getTag(): ?string
+    #[OA\Property(
+        property: "cardUniqueNumber",
+        description: "Total card number unique in the Deck",
+        type: "integer",
+        nullable: false
+    )]
+    #[Groups(["deck_user_list"])]
+    public function getCardUniqueNumber(): int
     {
-        return $this->tag;
+        return $this->cardsUnique->count();
     }
 
-    public function setTag(?string $tag): static
+    /**
+     * @return Collection<int, Card>
+     */
+    public function getCardsUnique(): Collection
     {
-        $this->tag = $tag;
+        return $this->cardsUnique;
+    }
+
+    public function addCardsUnique(Card $cardsUnique): static
+    {
+        if (!$this->cardsUnique->contains($cardsUnique)) {
+            $this->cardsUnique->add($cardsUnique);
+            $cardsUnique->addDeck($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCardsUnique(Card $cardsUnique): static
+    {
+        if ($this->cardsUnique->removeElement($cardsUnique)) {
+            $cardsUnique->removeDeck($this);
+        }
+
+        return $this;
+    }
+
+    #[OA\Property(
+        property: "cardMainDeckNumber",
+        description: "Total card number in the main-deck",
+        type: "integer",
+        nullable: false
+    )]
+    #[Groups(["deck_user_list"])]
+    public function getCardMainDeckNumber(): int
+    {
+        return $this->cardMainDecks->count();
+    }
+
+    /**
+     * @return Collection<int, CardMainDeck>
+     */
+    public function getCardMainDecks(): Collection
+    {
+        return $this->cardMainDecks;
+    }
+
+    public function addCardMainDeck(CardMainDeck $cardMainDeck): static
+    {
+        if (!$this->cardMainDecks->contains($cardMainDeck)) {
+            $this->cardMainDecks->add($cardMainDeck);
+            $cardMainDeck->setDeck($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCardMainDeck(CardMainDeck $cardMainDeck): static
+    {
+        if ($this->cardMainDecks->removeElement($cardMainDeck)) {
+            // set the owning side to null (unless already changed)
+            if ($cardMainDeck->getDeck() === $this) {
+                $cardMainDeck->setDeck(null);
+            }
+        }
+
+        return $this;
+    }
+
+    #[OA\Property(
+        property: "cardExtraDeckNumber",
+        description: "Total card number in the extra-deck",
+        type: "integer",
+        nullable: false
+    )]
+    #[Groups(["deck_user_list"])]
+    public function getCardExtraDeckNumber(): int
+    {
+        return $this->cardExtraDecks->count();
+    }
+
+    /**
+     * @return Collection<int, CardExtraDeck>
+     */
+    public function getCardExtraDecks(): Collection
+    {
+        return $this->cardExtraDecks;
+    }
+
+    public function addCardExtraDeck(CardExtraDeck $cardExtraDeck): static
+    {
+        if (!$this->cardExtraDecks->contains($cardExtraDeck)) {
+            $this->cardExtraDecks->add($cardExtraDeck);
+            $cardExtraDeck->setDeck($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCardExtraDeck(CardExtraDeck $cardExtraDeck): static
+    {
+        if ($this->cardExtraDecks->removeElement($cardExtraDeck)) {
+            // set the owning side to null (unless already changed)
+            if ($cardExtraDeck->getDeck() === $this) {
+                $cardExtraDeck->setDeck(null);
+            }
+        }
+
+        return $this;
+    }
+
+    #[OA\Property(
+        property: "cardSideDeckNumber",
+        description: "Total card number in the side-deck",
+        type: "integer",
+        nullable: false
+    )]
+    #[Groups(["deck_user_list"])]
+    public function getCardSideDeckNumber(): int
+    {
+        return $this->cardSideDecks->count();
+    }
+
+    /**
+     * @return Collection<int, CardSideDeck>
+     */
+    public function getCardSideDecks(): Collection
+    {
+        return $this->cardSideDecks;
+    }
+
+    public function addCardSideDeck(CardSideDeck $cardSideDeck): static
+    {
+        if (!$this->cardSideDecks->contains($cardSideDeck)) {
+            $this->cardSideDecks->add($cardSideDeck);
+            $cardSideDeck->setDeck($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCardSideDeck(CardSideDeck $cardSideDeck): static
+    {
+        if ($this->cardSideDecks->removeElement($cardSideDeck)) {
+            // set the owning side to null (unless already changed)
+            if ($cardSideDeck->getDeck() === $this) {
+                $cardSideDeck->setDeck(null);
+            }
+        }
+
+        return $this;
+    }
+
+    #[OA\Property(
+        property: "artworkUrl",
+        description: "URL for the Deck Artwork",
+        type: "string",
+        nullable: true
+    )]
+    #[Groups(["deck_user_list"])]
+    public function getArtworkUrl(): ?string
+    {
+        return $this->artwork?->getArtworkUrl();
+
+    }
+
+    public function getArtwork(): ?CardPicture
+    {
+        return $this->artwork;
+    }
+
+    public function setArtwork(?CardPicture $artwork): static
+    {
+        $this->artwork = $artwork;
 
         return $this;
     }
