b1c5467dc9e4e41b08d76ec80dafafc31258afc5
isma91 <ismaydogmus@gmail.com>
2024-03-18 17:28:40 +0100

    add ORMSearch in deck orm

--- src/Service/Tool/Deck/ORM.php
+++ src/Service/Tool/Deck/ORM.php
@@ -7,6 +7,7 @@ use App\Entity\Deck;
 use App\Entity\User;
 use App\Repository\DeckRepository;
 use App\Service\Tool\Abstract\AbstractORM;
+use App\Service\Tool\ORMSearch;
 use App\Service\Tool\ORMSlugName;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
@@ -14,19 +15,24 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 class ORM extends AbstractORM
 {
     protected ORMSlugName $ORMSlugName;
+    protected ORMSearch $ORMSearch;
 
-    public function __construct(EntityManagerInterface $em, DeckRepository $repository, SluggerInterface $slugger)
+    public function __construct(
+        EntityManagerInterface $em,
+        DeckRepository $repository,
+        SluggerInterface $slugger
+    )
     {
+        $this->ORMSearch = new ORMSearch($repository);
         $this->ORMSlugName = new ORMSlugName($repository, $slugger);
         parent::__construct($em, $repository);
     }
 
     /**
-     * @param User $user
-     * @return Deck[]
+     * @return ORMSearch
      */
-    public function findByUser(User $user): array
+    public function getORMSearch(): ORMSearch
     {
-        return $this->repository->findBy(["user" => $user]);
+        return $this->ORMSearch;
     }
 }
