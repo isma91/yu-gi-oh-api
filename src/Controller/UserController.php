9aa706ccf19456f18fa30a51bf9d56941a8a4637
isma91 <ismaydogmus@gmail.com>
2024-03-22 14:47:34 +0100

    create route for edit password & username

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+use Symfony\Component\Routing\Requirement\Requirement;
 
 #[OA\Tag(name: "User")]
 #[Route('/user', name: 'api_user')]
@@ -134,5 +135,122 @@ class UserController extends CustomAbstractController
             return $this->sendError($error, $errorDebug);
         }
         return $this->sendSuccess("User successfully logout.");
+    }
+
+    #[OA\Response(
+        response: SymfonyResponse::HTTP_OK,
+        description: "User's password updated successfully",
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(property: "success", type: "string"),
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: SymfonyResponse::HTTP_BAD_REQUEST,
+        description: "Error when updating User's password",
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(property: "error", type: "string"),
+            ]
+        )
+    )]
+    #[OA\RequestBody(
+        request: "UserEditPasswordRequest",
+        required: true,
+        content: new OA\MediaType(
+            mediaType: "multipart/form-data",
+            schema: new OA\Schema(
+                required: ["currentPassword", "password", "confirmPassword"],
+                properties: [
+                    new OA\Property(
+                        property: "currentPassword",
+                        description: "The User's current password",
+                        type: "string"
+                    ),
+                    new OA\Property(
+                        property: "password",
+                        description: "The new password",
+                        type: "string"
+                    ),
+                    new OA\Property(
+                        property: "currentPassword",
+                        description: "The same new password, to avoid bad password edit",
+                        type: "string"
+                    ),
+                ]
+            )
+        )
+    )]
+    #[Security(name: "Bearer")]
+    #[Route("/edit-password", name: "_edit_password", methods: ["POST"])]
+    public function editPassword(Request $request, UserService $userService):JsonResponse
+    {
+        $waitedParameter = [
+            "currentPassword" => "password",
+            "password" => "password",
+            "confirmPassword" => "password",
+        ];
+        [
+            "error" => $error,
+            "parameter" => $parameter,
+            "jwt" => $jwt,
+        ] = $this->checkRequestParameter($request, $waitedParameter);
+        if ($error !== "") {
+            return $this->sendError($error);
+        }
+        [
+            "error" => $error,
+            "errorDebug" => $errorDebug
+        ] = $userService->editPassword($jwt, $parameter);
+        if ($error !== "") {
+            return $this->sendError($error, $errorDebug);
+        }
+        return $this->sendSuccess("Your password updated successfully.");
+    }
+
+    #[OA\Response(
+        response: SymfonyResponse::HTTP_OK,
+        description: "Username updated successfully",
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(property: "success", type: "string"),
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: SymfonyResponse::HTTP_BAD_REQUEST,
+        description: "Error when updating your username.",
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(property: "error", type: "string"),
+            ]
+        )
+    )]
+    #[OA\Parameter(
+        name: "username",
+        description: "New username of the User, must be at least 3 character long",
+        in: "path",
+        required: true,
+        schema: new OA\Schema(type: "string")
+    )]
+    #[Security(name: "Bearer")]
+    #[Route(
+        "/edit-username/{username}",
+        name: "_edit_username",
+        requirements: ["username" => Requirement::CATCH_ALL],
+        methods: ["PUT"]
+    )]
+    public function editUsername(string $username, Request $request, UserService $userService):JsonResponse
+    {
+        $jwt = $this->getJwt($request);
+        [
+            "error" => $error,
+            "errorDebug" => $errorDebug
+        ] = $userService->editUsername($jwt, $username);
+        if ($error !== "") {
+            return $this->sendError($error, $errorDebug);
+        }
+        return $this->sendSuccess("Your username is updated successfully.");
     }
 }
