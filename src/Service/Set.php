322cc73035eed2e7e596b24db9bc5b0234474b8e
isma91 <ismaydogmus@gmail.com>
2024-03-23 11:40:00 +0100

    create route to get info from a Set

--- src/Service/Set.php
+++ src/Service/Set.php
@@ -24,4 +24,43 @@ class Set
     {
         return $this->setORMService;
     }
+
+    /**
+     * @param string $jwt
+     * @param int $id
+     * @return array[
+     *  "error" => string,
+     *  "errorDebug" => string,
+     * "set" => array[mixed]
+     * ]
+     */
+    public function getInfo(string $jwt, int $id): array
+    {
+        $response = [...$this->customGenericService->getEmptyReturnResponse(), "set" => []];
+        try {
+            $user = $this->customGenericService->customGenericCheckJwt($jwt);
+            if ($user === NULL) {
+                $response["error"] = "No user found.";
+                return $response;
+            }
+            $set = $this->setORMService->findById($id);
+            if ($set === NULL) {
+                $response["error"] = "Set not found.";
+                return $response;
+            }
+            $setSerialize = $this->customGenericService->getInfoSerialize([$set], ["set_info"])[0];
+            $cardSets = $set->getCardSets();
+            foreach ($cardSets as $key => $cardSet) {
+                $cardSerialize = $this->customGenericService->getInfoSerialize([$cardSet->getCard()], ["card_info"])[0];
+                $setSerialize["cardSets"][$key]["card"] = $cardSerialize;
+            }
+            $response["set"] = $setSerialize;
+            //$this->customGenericService->addInfoLogFromDebugBacktrace();
+        } catch (Exception $e) {
+            $this->customGenericService->addExceptionLog($e);
+            $response["errorDebug"] = sprintf('Exception : %s', $e->getMessage());
+            $response["error"] = "Error while getting Set info.";
+        }
+        return $response;
+    }
 }
