0db7118147f43e569c034f3d93f44ec51984cedf
isma91 <ismaydogmus@gmail.com>
2024-03-28 16:15:47 +0100

    add ormSearch entity type for cardCollection ORM

--- src/Service/Tool/CardCollection/ORM.php
+++ src/Service/Tool/CardCollection/ORM.php
@@ -5,6 +5,7 @@ namespace App\Service\Tool\CardCollection;
 
 use App\Repository\CardCollectionRepository;
 use App\Service\Tool\Abstract\AbstractORM;
+use App\Service\Tool\ORMSearch;
 use App\Service\Tool\ORMSlugName;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
@@ -12,10 +13,20 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 class ORM extends AbstractORM
 {
     protected ORMSlugName $ORMSlugName;
+    protected ORMSearch $ORMSearch;
 
     public function __construct(EntityManagerInterface $em, CardCollectionRepository $repository, SluggerInterface $slugger)
     {
         $this->ORMSlugName = new ORMSlugName($repository, $slugger);
+        $this->ORMSearch = new ORMSearch($repository);
         parent::__construct($em, $repository);
+    }
+
+    /**
+     * @return ORMSearch
+     */
+    public function getORMSearch(): ORMSearch
+    {
+        return $this->ORMSearch;
     }
 }
