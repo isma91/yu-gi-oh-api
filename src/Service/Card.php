edc003256980228ece913bd12a434cc1a144689a
isma91 <ismaydogmus@gmail.com>
2024-04-06 15:31:28 +0200

    fix when checking if user have right to display deck info

--- src/Service/Card.php
+++ src/Service/Card.php
@@ -63,7 +63,7 @@ class Card
                 if (empty($deckUser) === TRUE) {
                     continue;
                 }
-                if ($isAdmin === FALSE && $isPublic === FALSE && $deckUser->getUsername() !== $user->getUsername()) {
+                if ($isAdmin === FALSE && $isPublic === FALSE && $deckUser[0]->getUsername() !== $user->getUsername()) {
                     continue;
                 }
                 $deckList[] = $deckInfo;
