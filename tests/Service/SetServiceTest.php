f4c6fe27d3926f5e2e46314aa4b85891abc7ebd6
isma91 <ismaydogmus@gmail.com>
2024-04-08 15:08:28 +0200

    add set get info test

--- tests/Service/SetServiceTest.php
+++ tests/Service/SetServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Service;
 
+use App\Repository\SetRepository;
 use App\Service\Set;
 use App\Service\Tool\Set\ORM;
 use Doctrine\ORM\EntityNotFoundException;
@@ -27,5 +28,16 @@ class SetServiceTest extends AbstractTestService
      */
     public function testSetGetInfo(): void
     {
+        $setRepository = self::getService(SetRepository::class);
+        $sets = $setRepository->findBy(["code" => "RA01"]);
+        if (empty($sets) === TRUE) {
+            throw new EntityNotFoundException("Set with coe 'RA01' not found, maybe you forgot to run the Import before testing ??");
+        }
+        [
+            "error" => $error,
+            "set" => $setInfo
+        ] = $this->service->getInfo($sets[0]);
+        $this->assertEmpty($error);
+        $this->assertSame($sets[0]->getId(), $setInfo["id"]);
     }
 }
