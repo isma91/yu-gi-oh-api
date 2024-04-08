11c0f1d55c1287fbc850751debdd0c7a0f8062a3
isma91 <ismaydogmus@gmail.com>
2024-04-08 11:55:18 +0200

    use map entity to avoid using ORM

--- src/Service/CardPicture.php
+++ src/Service/CardPicture.php
@@ -2,62 +2,53 @@
 
 namespace App\Service;
 
+use App\Entity\Card as CardEntity;
+use App\Entity\CardPicture as CardPictureEntity;
 use App\Service\Tool\CardPicture\File as CardPictureFileService;
-use App\Service\Tool\Card\ORM as CardORMService;
 use Exception;
 
 class CardPicture
 {
     private CardPictureFileService $cardPictureFileService;
-    private CardORMService $cardORMService;
 
     public function __construct(
         CustomGeneric $customGenericService,
-        CardPictureFileService $cardPictureFileService,
-        CardORMService $cardORMService
+        CardPictureFileService $cardPictureFileService
     )
     {
         $this->cardPictureFileService = $cardPictureFileService;
-        $this->cardORMService = $cardORMService;
     }
 
     /**
-     * @param string $uuid
-     * @param int $idYGO
+     * @param CardEntity $card
+     * @param CardPictureEntity $cardPicture
      * @param string $name
      * @return string
      */
-    public function getPicture(string $uuid, int $idYGO, string $name): string
+    public function getPicture(CardEntity $card, CardPictureEntity $cardPicture, string $name): string
     {
         $file = $this->cardPictureFileService->getDefaultFilePath();
         try {
-            $cardEntity = $this->cardORMService->findByUuid($uuid);
-            if ($cardEntity === NULL) {
+            $cardPictureCard = $cardPicture->getCard();
+            $cardUuid = $card->getUuid();
+            if (
+                $cardPictureCard === NULL ||
+                $cardUuid === NULL ||
+                $cardPictureCard->getId() !== $card->getId()
+            ) {
                 return $file;
             }
-            $cardPictures = $cardEntity->getPictures();
-            if ($cardPictures->count() <= 0) {
+            $idYGO = $cardPicture->getIdYGO();
+            if ($cardPicture->getPicture() === $name) {
+                $fieldName = "picture";
+            } elseif ($cardPicture->getPictureSmall() === $name) {
+                $fieldName = "pictureSmall";
+            } elseif ($cardPicture->getArtwork() === $name) {
+                $fieldName = "artwork";
+            } else {
                 return $file;
             }
-            $fieldName = NULL;
-            foreach ($cardPictures as $cardPicture) {
-                if ($cardPicture->getIdYGO() === $idYGO) {
-                    if ($cardPicture->getPicture() === $name) {
-                        $fieldName = "picture";
-                    } elseif ($cardPicture->getPictureSmall() === $name) {
-                        $fieldName = "pictureSmall";
-                    } elseif ($cardPicture->getArtwork() === $name) {
-                        $fieldName = "artwork";
-                    }
-                }
-                if ($fieldName !== NULL) {
-                    break;
-                }
-            }
-            if ($fieldName === NULL) {
-                return $file;
-            }
-            $option = ["uuid" => $uuid, "idYGO" => $idYGO, "name" => $name];
+            $option = ["uuid" => $cardUuid->__toString(), "idYGO" => $idYGO, "name" => $name];
             $file = $this->cardPictureFileService->getFilePath($fieldName, $option);
         } catch (Exception $e) {
             return $file;
